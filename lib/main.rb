9542beb7110a84e3e7d32c8c870b4262816e91ce
ktoodo <kristjan.toodo@gmail.com>
2021-12-14 11:22:19 +0200

    Hangman basics

--- lib/main.rb
+++ lib/main.rb
@@ -4,30 +4,23 @@ module WordGenerator
   end
 end
 
-#evaluate guess and go through game
-class Hangman
-  def initialize
-    
-  end
-end
+module Display
 
-#make guesses
-class Player
-  def initialize
-    
-  end
-end
-
-#display state
-class Display
-  def initialize
-    
+  def hidden_word(secret_word, guessed_letters)
+    displayed_word = ""
+    secret_word.split("").each do |letter|
+      if guessed_letters.include?(letter)
+        displayed_word << " #{letter} "
+      else
+        displayed_word << " _ "
+      end
+    end
+    displayed_word
   end
 
-  def round_info()
-    puts "\nWord to guess: "
-    puts "Incorrect letters: "
-    puts "Letters to choose from: "
+  def round_info(secret_word, guessed_letters)
+    puts "\nWord to guess: #{self.hidden_word(secret_word, guessed_letters)}"
+    puts "Incorrect letters: #{(guessed_letters - secret_word.split("")).join(", ")}"
   end
 
   def graphics(incorrect)
@@ -99,8 +92,74 @@ class Display
   end
 end
 
-hangman = Display.new
-for i in 0..7 do
-  puts hangman.graphics(i)
+#make guesses
+class Player
+  attr_accessor :guessed_letters
+
+  def initialize
+    @guessed_letters = []
+  end
+
+  def guess_letter(available_letters)
+    puts "\nGuess a letter"
+    letter = gets.chomp.downcase
+    if @guessed_letters.include?(letter)
+      guess_letter(available_letters)
+    elsif available_letters.include?(letter)
+      @guessed_letters += [letter]
+    else
+      guess_letter(available_letters)
+    end
+  end
+
+  def lives(secret_word, guessed_letters)
+    incorrect = guessed_letters.reject { |letter| secret_word.include?(letter)}
+    incorrect.length
+  end
 end
 
+#evaluate guess and go through game
+class Hangman
+  include Display, WordGenerator
+  attr_accessor :available_letters
+
+  def initialize
+    @available_letters = ('a'..'z').to_a
+    @secret_word = word_generator
+    @player = Player.new()
+    play_game
+  end
+
+  def play_game
+    until game_won || game_lost
+      play_round
+    end
+  end
+
+  def play_round
+    puts graphics(@player.lives(@secret_word, @player.guessed_letters))
+    round_info(@secret_word, @player.guessed_letters)
+    p @secret_word
+    @player.guess_letter(@available_letters)
+  end
+
+  def game_won
+    if @secret_word.split("").all? {|letter| @player.guessed_letters.include?(letter)}
+      puts "You won, the word was '#{@secret_word}'"
+      true
+    else
+      false
+    end
+  end
+
+  def game_lost
+    if @player.lives(@secret_word, @player.guessed_letters) == 6
+      puts graphics(@player.lives(@secret_word, @player.guessed_letters))
+      return true
+    else
+      false
+    end
+  end
+end
+
+game = Hangman.new()
